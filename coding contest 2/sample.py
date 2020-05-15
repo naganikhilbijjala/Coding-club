b5fc03669da18871f32e72ddb62b35358e0eb35e
nikhil74 <naganikhil.bijjala@gmail.com>
2020-05-15 12:51:45 +0530

    Before adding thirsty module in else

---
+++ coding contest 2/sample.py
@@ -0,0 +1,11 @@
+'''input
+yes
+'''
+hungry = input("Are you hungry?")
+if hungry=="yes":
+	print("eat samosa")
+	print("eat pizza")
+	print("eat burger")
+	print("eat fries")
+else:
+	print("do your homework")
