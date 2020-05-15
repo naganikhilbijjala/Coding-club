b5d27555b018063be2d3fc7e6d25f0409de9b516
nikhil74 <naganikhil.bijjala@gmail.com>
2020-05-15 10:41:04 +0530

    learning about branches

--- coding contest 2/sumArray.c
+++ coding contest 2/sumArray.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <math.h>
 int main(){
     int N,A[101],sum=0;
     scanf("%d",&N);
-    for(int i=1; i<=N; i++)
+    for(int i=1; i<=N; i++){
         scanf("%d",&A[i]);
+    }
     for(int i=1; i<=N; i++){
         sum+=A[i];
         printf("%d ",sum);
